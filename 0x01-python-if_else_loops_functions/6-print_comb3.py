3b07f7e2f7c11dae167c5feb752412c0213c3b37
Elbadil <elbaliadil@gmail.com>
2023-06-06 09:55:49 -0700

    Print combination of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            if i != 8 or j != 9:
+                print("{0:d}".format(i), end='')
+                print("{0:d}".format(j), end=', ')
+            else:
+                print("{0:d}".format(89))
