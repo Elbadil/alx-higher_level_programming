7d362c2f5e06845b31b9f83f58d2e32c051c4111
Elbadil <elbaliadil@gmail.com>
2023-06-08 06:42:55 -0700

    Print command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     args = len(sys.argv) - 1
     if args == 0:
         print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
     else:
         print("{} arguments:".format(args))
     for i in range(args):
