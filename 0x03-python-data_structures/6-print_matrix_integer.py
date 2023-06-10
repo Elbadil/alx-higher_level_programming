430b07ad688840d8148784bd8c265fb1a967b7d8
Elbadil <elbaliadil@gmail.com>
2023-06-10 05:21:44 -0700

    Matrix of Integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for inner_list in matrix:
-        numbers_joined = ' '.join(map(str, inner_list))
-        print("{}".format(numbers_joined))
+    for in_list in matrix:
+        for number in in_list:
+            if number != in_list[-1]:
+                print("{:d} ".format(number), end='')
+            else:
+                print("{:d}".format(number), end='')
+        print()
