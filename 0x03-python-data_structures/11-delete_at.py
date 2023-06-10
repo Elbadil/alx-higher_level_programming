3afddd6d842f9e8f02d753bceb3944cee63310de
Elbadil <elbaliadil@gmail.com>
2023-06-10 08:10:51 -0700

    Delete at index in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    list_size = len(my_list) - 1
+    if idx < 0:
+        return my_list
+
+    for num in range(list_size):
+        if num == idx:
+            del my_list[num]
+    return my_list
