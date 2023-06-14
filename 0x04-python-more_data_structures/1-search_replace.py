1cc54e0109a113619cdfedddda98a913147eb520
Elbadil <elbaliadil@gmail.com>
2023-06-14 06:38:30 -0700

    Replace element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     rplced_list = my_list.copy()
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         if rplced_list[i] == search:
             rplced_list[i] = replace
     return rplced_list
