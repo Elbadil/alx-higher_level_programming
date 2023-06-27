52b84110229e0cd77d0633508a6bd2e06bd98fd8
Elbadil <elbaliadil@gmail.com>
2023-06-27 07:05:15 -0700

    Class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a Class Called Square"""
+
+
+class Square:
+    """Represent a square with a private instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
