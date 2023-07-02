dfa2f2c236e1333e51ad78b773104def6e9e8ffe
Elbadil <elbaliadil@gmail.com>
2023-07-02 15:39:40 -0700

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,14 +4,15 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Defining unittest for the function max_integer(list=[])"""
-    
+
     def test_sorted_list(self):
         """testing max_integer on a list with sorted numbers"""
         int_list = [20, 21, 22, 23]
         self.assertEqual(max_integer(int_list), 23)
-    
+
     def test_non_sorted_list(self):
         """testing max_integer on a list with non-sorted numbers"""
         int_list = [102, 98, 3, 46]
@@ -26,7 +27,7 @@ class TestMaxInteger(unittest.TestCase):
         """testing max_integer on an empty list"""
         int_list = []
         self.assertEqual(max_integer(int_list), None)
-    
+
     def test_mixed_num_list(self):
         """testing max_integer on a list eith mixed numbers
         of negative and positive numbers
@@ -38,7 +39,7 @@ class TestMaxInteger(unittest.TestCase):
         """testing max_integer on a list with floating_point numbers"""
         float_list = [15.8, 25.7, 2.8, 87.7]
         self.assertEqual(max_integer(float_list), 87.7)
-    
+
     def test_duplicate_num_list(self):
         """testing max_integer on a list with duplicated numbers"""
         int_list = [2, 10, 2, 22, 10, 22]
@@ -48,12 +49,12 @@ class TestMaxInteger(unittest.TestCase):
         """testing max_integer on a list with range of numbers"""
         int_list = list(range(2**4))
         self.assertEqual(max_integer(int_list), 15)
-    
+
     def test_single_element_list(self):
         """testing max_integer on a list with a single element"""
         int_list = [55]
         self.assertEqual(max_integer(int_list), 55)
-    
+
     def test_ints_floats_list(self):
         """testing max_integer on a list with a ints and floats"""
         int_float_list = [55, 66.7, 42, 2.4, 27]
@@ -68,7 +69,7 @@ class TestMaxInteger(unittest.TestCase):
         """testing max_integer on a string"""
         string = "Hello There"
         self.assertEqual(max_integer(string), "r")
-    
+
     def test__empty_string(self):
         """testing max_integer on an empty string"""
         string = ""
