e2cda43fb5fcc0ce2a9f4913860277b8fdbe4fd0
Elbadil <elbaliadil@gmail.com>
2023-07-10 14:36:43 -0700

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+"""Defining a class called MyInt"""
+
+
+class MyInt(int):
+    """class MyInt that inherits from int"""
+
+    def __init__(self, val):
+        """Instantiates object"""
+        self.value = val
+
+    def __ne__(self, var):
+        """inverts the != operator"""
+        return self.value == var
+
+    def __eq__(self, var):
+        """inverts the == operator"""
+        return self.value != var
