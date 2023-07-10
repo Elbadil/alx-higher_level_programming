3410f6b3708f538dc32d40e383639f7b5c2573a4
Elbadil <elbaliadil@gmail.com>
2023-07-10 13:39:04 -0700

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defining a class called BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class Basegeometry."""
+
+    def area(self):
+        """area function not yet implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validator function checks if value is an integer.
+
+        Args:
+            name: The name of the parameter.
+            value: The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class rectangle that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+
+        Args:
+            width: The width of the new Rectangle.
+            height: The height of the new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area function returns the area of a rectangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """string representation of the method area"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
