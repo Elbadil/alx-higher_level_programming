cd8e049c653446450db1dd5e9a4357fbad5595ee
Elbadil <elbaliadil@gmail.com>
2023-07-11 12:27:15 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defining a class called Student"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return vars(self)
+
+        return {a: getattr(self, attr) for a in attrs if hasattr(self, a)}
