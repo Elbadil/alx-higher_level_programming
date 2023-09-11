41b73e025f962e7e2cd36c01dcb22a65e659f429
Elbadil <elbaliadil@gmail.com>
2023-09-11 14:50:16 +0100

    CMD Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,8 @@ const argv = process.argv;
 
 if (argv.length < 3) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
