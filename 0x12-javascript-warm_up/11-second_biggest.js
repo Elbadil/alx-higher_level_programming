b1b63aed8645ea68e4d3e9a42c7338fffe6dbbbd
Elbadil <elbaliadil@gmail.com>
2023-09-11 18:25:17 +0100

    Second largest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argv = process.argv;
+
+if (argv.length <= 3) {
+  console.log(0);
+}
+
+let largestNum = -Infinity;
+let secondLarge = -Infinity;
+
+for (let i = 2; i < argv.length; i++) {
+  if (parseInt(argv[i]) > largestNum) {
+    largestNum = parseInt(argv[i]);
+  }
+}
+
+for (let j = 2; j < argv.length; j++) {
+  if (parseInt(argv[j]) > secondLarge && parseInt(argv[j]) !== largestNum) {
+    secondLarge = parseInt(argv[j]);
+  }
+}
+
+console.log(secondLarge);
