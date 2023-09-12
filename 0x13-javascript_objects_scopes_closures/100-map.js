d1aede19997b71c88145f447025d3363feee6774
Elbadil <elbaliadil@gmail.com>
2023-09-12 18:13:43 +0100

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,5 @@
 const list = require('./100-data').list;
 
 console.log(list);
-const newList = list.map((x) => x * list.indexOf(x));
 
-console.log(newList);
+console.log(list.map((x) => x * list.indexOf(x)));
