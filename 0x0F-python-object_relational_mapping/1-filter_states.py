8d9219d192aa21465e7e0f959038bde9cc8df47c
Elbadil <elbaliadil@gmail.com>
2023-09-14 20:46:52 +0100

    Filter N states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = conn.cursor()
+    n_states = "SELECT * FROM states WHERE name REGEXP '^N'"
+    cursor.execute(n_states)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+    
+    conn.close()
