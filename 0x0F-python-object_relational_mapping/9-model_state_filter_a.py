592da930aed9303ce07d9a263763646e6a8ff449
Elbadil <elbaliadil@gmail.com>
2023-09-15 19:16:27 +0100

    Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""SQLAlchemy that connects a python script to a database"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    link = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    engine = create_engine(link)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    a_states = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in a_states:
+        print(f'{state.id}: {state.name}')
