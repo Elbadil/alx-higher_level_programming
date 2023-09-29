207ee00cb8e87946b216ba9a0bf71598cbb1674d
Elbadil <elbaliadil@gmail.com>
2023-09-29 11:26:45 -0700

    Error code with requests module

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Python script that sends a request to a URL, displays the body of the reponse
-and handles errors"""
+"""Python script that sends a request to a URL, displays the body
+of the reponse and handles errors"""
 import requests
 from sys import argv
 
@@ -8,6 +8,7 @@ from sys import argv
 if __name__ == "__main__":
     try:
         req = requests.get(argv[1])
+        req.raise_for_status()
         print(req.text)
-    except requests.HTTPError as e:
-        print(e.status_code)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {e.response.status_code}")
