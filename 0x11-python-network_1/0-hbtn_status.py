bb7b47c4d6a28e9f87bedbd54e00905133b87394
Elbadil <elbaliadil@gmail.com>
2023-09-29 16:26:26 +0100

    URL status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches a URL"""
+from urllib import request
+
+
+with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+    data = resp.read()
+    print("Body response:\n\t" +
+          f"- type: {type(data)}\n\t"+
+          f"- content: {data}\n\t"+
+          f"- utf8 content: {data.decode('UTF-8')}")
