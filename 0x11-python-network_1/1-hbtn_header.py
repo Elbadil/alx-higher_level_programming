52ac6b91a8c2024bfdd1b08c1a89514cd223baba
Elbadil <elbaliadil@gmail.com>
2023-09-29 16:46:25 +0100

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Python scrpt that sends a request to a URL and displays the value
 of X-Request-Id"""
-import urllib.request
-import sys
+from urllib import request
+from sys import argv
 
-
-with urllib.request.urlopen(sys.argv[1]) as resp:
-    headers = resp.headers
-    for header, value in headers.items():
-        if header == "X-Request-Id":
-            print(headers[header])
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as resp:
+        headers = resp.headers
+        for header, value in headers.items():
+            if header == "X-Request-Id":
+                print(headers[header])
