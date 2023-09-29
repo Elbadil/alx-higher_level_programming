eb7debcc81b365c2d1c4f855e9e95fcf937a80ba
Elbadil <elbaliadil@gmail.com>
2023-09-29 11:09:32 -0700

    POST an email with requests Module

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,4 @@ from sys import argv
 
 if __name__ == "__main__":
     req_post = requests.post(argv[1], data={'email': argv[2]})
-    req_get = requests.get(argv[1])
-    print(req_get.text)
+    print(req_post.text)
