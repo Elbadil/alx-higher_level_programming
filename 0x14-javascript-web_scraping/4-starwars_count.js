461746ccc2ee05bccce4076e39e6a83f8f09bcd3
Elbadil <elbaliadil@gmail.com>
2023-10-24 16:00:20 +0100

    Star wars Wedge Antilles Roles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argv = process.argv;
+const request = require('request');
+
+const WedgeAntillesID = 18;
+const filmsURL = argv[2];
+const charURL = `https://swapi-api.alx-tools.com/api/people/${WedgeAntillesID}/`;
+
+request(filmsURL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    let counter = 0;
+    const filmsData = JSON.parse(body);
+    for (const film of filmsData.results) {
+      for (const character of film.characters) {
+        if (character === charURL) {
+          counter += 1;
+        }
+      }
+    }
+    console.log(counter);
+  }
+});
