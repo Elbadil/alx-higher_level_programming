1465c6d89bb13c2d6c76638d5edc7c47728bf72b
Elbadil <elbaliadil@gmail.com>
2023-11-01 17:58:55 +0100

    Say Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,14 @@
+// fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+// and displays the value of hello from that fetch in the HTML tag DIV#hello
+
+$.ajax({
+  type: 'GET',
+  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+  dataType: 'json',
+  success: function (greetings) {
+    $('DIV#hello').text(greetings.hello);
+  },
+  error: function () {
+    console.error('The translation of hello is not found');
+  }
+});
